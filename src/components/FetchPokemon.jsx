aea29f943461c156ebe3b852509b82d75e741617
Fiorella Aquino <fiorellaaquino05@gmail.com>
2023-02-03 20:00:06 -0300

    primer commit

--- src/components/FetchPokemon.jsx
+++ src/components/FetchPokemon.jsx
@@ -1,18 +1,14 @@
 import React, { useState } from 'react'
 import styles from './FetchPokemon.module.css'
+import axios from 'axios';
 
 const FetchPokemon = () => {
     const [pokemon, setPokemon]= useState([]);
 
     const fetchPkmn = () => {
-        fetch("https://pokeapi.co/api/v2/pokemon/")
-        .then(response => {
-            return response.json();
-        }).then(response => {
-            setPokemon(response.results.map(pokemon => pokemon.name));
-        }).catch(err => {
-            console.log(err);
-        })
+        axios.get("https://pokeapi.co/api/v2/pokemon/")
+            .then(response => {setPokemon(response.data.results.map(pokemon => pokemon.name))})
+            .catch(err => {console.log(err);})
         }
     return (
         <>
